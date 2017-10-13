dc4ef29b3b3274be956a4c398fb6a40327008457
Christian Estepa <chrisestepa@MacBook-Air-de-Christian.local>
2017-10-13 10:37:21 +0200

    cambios

--- server/public/main.bundle.js
+++ server/public/main.bundle.js
@@ -2010,16 +2010,16 @@ var _a;
 // The build system defaults to the dev environment which uses `environment.ts`, but if you do
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
-// export const environment = {
-//   production: true,
-//   BASEURL: ''
-// };
-//local
 // The file contents for the current environment will overwrite these during build.
 var environment = {
-    production: false,
-    BASEURL: 'http://localhost:3000'
+    production: true,
+    BASEURL: ''
 };
+//local
+// export const environment = {
+//   production: false,
+//   BASEURL: 'http://localhost:3000'
+// };
 //# sourceMappingURL=environment.js.map
 
 /***/ }),
